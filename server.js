846717543e1cadc917770f34482164b747a7509b
Najla Parker <najla08@gmail.com>
2022-01-24 17:03:09 -0700

    created choices and connection

---
+++ server.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employees_db',
+});
+
+connection.connect();
+
+function runEmployees() {
+    inquirer.prompt(
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'option',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add department',
+                'Add roles',
+                'Add employees',
+                'Update employee roles',
+                'Update employee manager',
+                'View employee by manager',
+                'Exit'
+            ]
+
